aab57f8de09b53d5ce8cd26cbb8414c552b0f9be
aloekun <56211144+aloekun@users.noreply.github.com>
2025-03-23 03:48:51 +0900

    LangChain, LangSmithを使う設定を追加

---
+++ chapter4/set_langsmith_api_key.py
@@ -0,0 +1,13 @@
+import os
+from google.colab import userdata
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = userdata.get("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = "agent-book"
+os.environ["OPENAI_API_KEY"] = userdata.get("OPENAI_API_KEY")
+
+# os.environ["LANGSMITH_TRACING"] = "true"
+# os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
+# os.environ["LANGSMITH_API_KEY"] = userdata.get("LANGCHAIN_API_KEY")
+# os.environ["LANGSMITH_PROJECT"] = "agent-book"
