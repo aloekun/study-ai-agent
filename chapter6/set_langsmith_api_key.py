0e8006f557ca02943c41e387ca035d80f0cd19f6
aloekun <56211144+aloekun@users.noreply.github.com>
2025-03-30 17:25:03 +0900

    6章で使うAPIキーを設定するスクリプトを追加

---
+++ chapter6/set_langsmith_api_key.py
@@ -0,0 +1,10 @@
+import os
+from google.colab import userdata
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = userdata.get("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = "agent-book"
+os.environ["OPENAI_API_KEY"] = userdata.get("OPENAI_API_KEY")
+os.environ["TAVILY_API_KEY"] = userdata.get("TAVILY_API_KEY")
+os.environ["COHERE_API_KEY"] = userdata.get("COHERE_API_KEY")
